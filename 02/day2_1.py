d674ee4e2cfd5494db9ad61ad533c73eb7839867
timothee.vargenau@student-cs.fr <timothee.vargenau@student-cs.fr>
2023-12-02 11:06:18 +0100

    first version of day 2 problem

---
+++ 02/day2_1.py
@@ -0,0 +1,22 @@
+COLORS = {"red":12,"green":13,"blue":14}
+
+def is_subset_possible(subset:str) -> bool:
+    for a in subset.split(', '):
+        number, color = a.split()
+        if COLORS[color] < int(number):
+            return False
+    return True
+
+def main():
+    with open("02/input.txt", "r") as f:
+        total = 0
+        for line in f.readlines():
+            game, subsets = line.split(":")
+            id = int(game.split()[1])
+            if all([is_subset_possible(subset) for subset in subsets.split(';')]):
+                total+= int(id)
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
